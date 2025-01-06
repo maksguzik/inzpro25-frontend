a2b3e311c7b41f99ea1c71dce53b2b138159b8f6
Goral <wojtek.goralewski1@gmail.com>
2025-01-06 12:11:46 +0100

    title for chart

--- inzpro25-frontend/src/Pages/Raports/SummaryRaportsPage/SummaryRaportsComponents/DeviceSummary/DeviceSummaryComponents/DeviceDowntimeInterface.jsx
+++ inzpro25-frontend/src/Pages/Raports/SummaryRaportsPage/SummaryRaportsComponents/DeviceSummary/DeviceSummaryComponents/DeviceDowntimeInterface.jsx
@@ -15,8 +15,8 @@ const DeviceDowntimeInterface = ({ deviceId}) => {
         return <DeviceDowntimeHeatMap deviceId={deviceId}/>;
       case "barchart":
         return <DeviceDowntimeBarChart deviceId={deviceId}/>;
-      case "grid":
-        return <DeviceDowntimeCalendarTwo month={0} year={2025} downtimes={downtimes} />;
+      // case "grid":
+      //   return <DeviceDowntimeCalendarTwo month={0} year={2025} downtimes={downtimes} />;
       default:
         return <DeviceDowntimeHeatMap deviceId={deviceId}/>;
     }
@@ -24,10 +24,11 @@ const DeviceDowntimeInterface = ({ deviceId}) => {
 
   return (
     <div>
+      <h3 style={{ marginRight: "8px", marginTop: "0px" }}>Device downtime days</h3>
       <div>
         <button style={{ marginRight: "8px", marginTop: "5px" }} className="crudButton greyButton paginationButton" onClick={() => setSelectedChart("heatmap")}>Heatmap</button>
         <button style={{ marginRight: "8px" }} className="crudButton greyButton paginationButton" onClick={() => setSelectedChart("barchart")}>Bar Chart</button>
-        <button className="crudButton greyButton paginationButton" onClick={() => setSelectedChart("grid")}>Calendar</button>
+        {/* <button className="crudButton greyButton paginationButton" onClick={() => setSelectedChart("grid")}>Calendar</button> */}
       </div>
 
       {renderSelectedChart()}
