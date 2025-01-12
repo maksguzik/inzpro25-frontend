f01935c7f0fb7f92ce837c05b37d3fdf565d95d7
Goral <wojtek.goralewski1@gmail.com>
2025-01-12 19:01:49 +0100

    added description for device id serach in raports

--- inzpro25-frontend/src/Pages/Raports/SummaryRaportsPage/SummaryRaportsComponents/DeviceSummary/DeviceSummary.jsx
+++ inzpro25-frontend/src/Pages/Raports/SummaryRaportsPage/SummaryRaportsComponents/DeviceSummary/DeviceSummary.jsx
@@ -88,6 +88,10 @@ function DeviceSummary() {
     <div className="deviceSummaryContainer">
 
       <div className="input-container">
+        <div className="filter-group2">
+          <label htmlFor="search-specific-device-id" className="search-label">
+            Search Specific Device ID:
+          </label>
         <input
           type="text"
           placeholder="Enter Device ID"
@@ -96,12 +100,14 @@ function DeviceSummary() {
           onKeyPress={handleKeyPress} 
           className="input-field"
         />
+        </div>
         <button
           onClick={handleSearch} 
           className="crudButton greyButton paginationButton"
         >
-          Search
+          Search Device
         </button>
+
       </div>
 
       {loading && <div className="loading">Loading data...</div>}
